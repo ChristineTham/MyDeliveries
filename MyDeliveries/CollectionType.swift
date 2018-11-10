9a2e2ef6829c63a388f355df1a98c1c2db5a7a55
Christine Tham <chris.tham@hellotham.com>
2018-11-10 17:17:13 +1100

    Initial Commit

---
+++ MyDeliveries/CollectionType.swift
@@ -0,0 +1,18 @@
+//
+// CollectionType.swift
+// MyDeliveries
+//
+// Created by SAP Cloud Platform SDK for iOS Assistant application on 10/11/18
+//
+
+import Foundation
+
+enum CollectionType: String {
+    case deliveryStatus = "DeliveryStatus"
+    case packages = "Packages"
+    case none = ""
+
+    static let all = [
+        deliveryStatus, packages,
+    ]
+}
